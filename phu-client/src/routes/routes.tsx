5ad38db7630644f5dda3e9b39489cc95fbf57e83
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-01-20 14:19:30 +0600

    Add new routes

--- phu-client/src/routes/routes.tsx
+++ phu-client/src/routes/routes.tsx
@@ -1,10 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import About from "../pages/About";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 
