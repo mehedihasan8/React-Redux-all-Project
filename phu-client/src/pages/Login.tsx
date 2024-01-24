5c93f6672b52c7e0511213ea3a851b4941a96842
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-01-24 23:15:49 +0600

    connect baseApi in backent

--- phu-client/src/pages/Login.tsx
+++ phu-client/src/pages/Login.tsx
@@ -1,8 +1,38 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "A-0001",
+      password: "admin123",
+    },
+  });
+
+  const [login, { data, isError }] = useLoginMutation();
+
+  console.log("data =>", data);
+  console.log("isError =>", isError);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    login(data);
+  };
+
   return (
-    <div>
-      <h1>Login page</h1>
-    </div>
+    <form action="" onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">Id:</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="id">password</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+
+      <Button htmlType="submit">Login</Button>
+    </form>
   );
 };
 
--- phu-client/src/redux/store.ts
+++ phu-client/src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
+import { baseApi } from "./api/baseApi";
 
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
     auth: authReducer,
   },
+  middleware: (getDefaultMIddlerware) =>
+    getDefaultMIddlerware().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
