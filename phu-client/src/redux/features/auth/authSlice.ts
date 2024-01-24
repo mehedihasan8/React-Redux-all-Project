1655ef202d543aff18d7b9e614082196836be707
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-01-24 22:22:53 +0600

    create authSlice

---
+++ phu-client/src/redux/features/auth/authSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type TInitialState = {
+  user: null | object;
+  token: null | object;
+};
+
+const initialState: TInitialState = {
+  user: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    Logout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setUser, Logout } = authSlice.actions;
+
+export default authSlice.reducer;
