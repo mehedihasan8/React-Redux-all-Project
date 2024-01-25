c40515ccb6aa3d2cc597bbc8388529cdb6c5d6d0
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-01-25 23:55:36 +0600

    solve error in auth props

--- phu-client/src/redux/api/baseApi.ts
+++ phu-client/src/redux/api/baseApi.ts
@@ -1,4 +1,11 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  DefinitionType,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 import { Logout, setUser } from "../features/auth/authSlice";
 
@@ -16,7 +23,12 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
-const baseQueryWithRefreshToken = async (ages, api, extraOptions) => {
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+> = async (ages, api, extraOptions): Promise<any> => {
   let result = await baseQuery(ages, api, extraOptions);
 
   if (result.error?.status === 401) {
