33f23ae9b3df0cce46fa466a6575d6ba830ab3b6
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-02-29 23:52:12 +0600

    today task done

--- first-project/src/app/utils/sendResponse.ts
+++ first-project/src/app/utils/sendResponse.ts
@@ -1,11 +1,18 @@
 import { Response } from 'express';
 
+type TMeta = {
+  limit: number;
+  page: number;
+  total: number;
+  totalPage: number;
+};
+
 type TResponse<T> = {
   statusCode: number;
   success: boolean;
   message?: string;
   data: T;
-  meta?: T;
+  meta?: TMeta;
 };
 
 const sendResponse = <T>(res: Response, data: TResponse<T>) => {
@@ -13,7 +20,7 @@ const sendResponse = <T>(res: Response, data: TResponse<T>) => {
     success: data.success,
     message: data.message,
     data: data.data,
-    meta: data?.data,
+    meta: data?.meta,
   });
 };
 
